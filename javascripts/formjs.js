524197e41c2990ff4f5551f30f9824986205984c
Omar <omarmandeeli@gmail.com>
2017-03-20 01:57:55 +0800

    update

--- javascripts/formjs.js
+++ javascripts/formjs.js
@@ -2,6 +2,39 @@
 
 */
 
+$(".dropdown dt a").on('click', function() {
+  $(".dropdown dd ul").slideToggle('fast');
+});
+
+$(".dropdown dd ul li a").on('click', function() {
+  $(".dropdown dd ul").hide();
+});
+
+function getSelectedValue(id) {
+  return $("#" + id).find("dt a span.value").html();
+}
+
+$(document).bind('click', function(e) {
+  var $clicked = $(e.target);
+  if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
+});
+
+$('.mutliSelect input[type="checkbox"]').on('click', function() {
+
+  var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
+    title = $(this).val() + ",";
+
+  if ($(this).is(':checked')) {
+    var html = '<span title="' + title + '">' + title + '</span>';
+    $('.multiSel').append(html);
+    $(".hida").hide();
+  } else {
+    $('span[title="' + title + '"]').remove();
+    var ret = $(".hida");
+    $('.dropdown dt a').append(ret);
+
+  }
+});
 
 WebFontConfig = {
     google: { families: [ 'Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic:latin' ] }
@@ -64,8 +97,10 @@ $( function() {
     $( "#datepicker" ).datepicker();
   } );
 
-$(document).ready(function(){
-    $('#foo_form input:checkbox').change(function(){
-       $('#bar_' + this.id.replace('foo_', '')).prop('checked', this.checked); 
-    });
-});
+
+/*
+  Dropdown with Multiple checkbox select with jQuery - May 27, 2013
+  (c) 2013 @ElmahdiMahmoud
+  license: http://www.opensource.org/licenses/mit-license.php
+*/
+
